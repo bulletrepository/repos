d5a61d73ce4daf64413fc52d5642cd11c820e34d
yangfeng <723139166@qq.com>
2020-10-01 02:51:40 +0800

    addding files

---
+++ bulletrepository.py
@@ -0,0 +1,25 @@
+import datetime
+
+def savelog(data):
+    now_time = datetime.datetime.now()
+    time_str = datetime.datetime.strftime(now_time,'%Y-%m-%d %H:%M:%S')
+    savelog_file = open(r'D:\Users\python\savelog.txt','a')
+    savelog_file.write(time_str + '\t' + data+'\n')
+    print(time_str + '\t' + data)
+    savelog_file.close()
+
+class Bulletrepository(object):
+    SAVEPATH = r'D:\Users\python\savelog.txt'
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
